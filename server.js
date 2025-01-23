b00d164fa86cd35a582d5a35bc833826e56b6c99
Mithil2603 <mithilsuthar2603@gmail.com>
2025-01-23 22:17:18 +0530

    new link added

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3001", "http://localhost:3000"],
+    origin: ["http://localhost:3001", "http://localhost:3000", "https://radheenterprise.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
